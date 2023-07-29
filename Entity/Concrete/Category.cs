08d17829c17d712dba1273b4930154085d5d9f25
Murad <murad.yunus.2017@gmail.com>
2023-07-29 16:12:54 +0400

    test commit

--- Entity/Concrete/Category.cs
+++ Entity/Concrete/Category.cs
@@ -5,6 +5,7 @@ namespace Entity.Concrete
     public class Category : IEntity
     {
         public int Id { get; set; }
+        public int Test { get; set; }
         public string Name { get; set; }
         public List<Course> Course { get; set; }
         public List<SubCategory> SubCategories { get; set; }
