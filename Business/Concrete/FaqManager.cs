379202751f4b2bb055a6c5fb0cac8b8f354849da
Murad <murad.yunus.2017@gmail.com>
2023-07-29 17:24:23 +0400

    test

---
+++ Business/Concrete/FaqManager.cs
@@ -0,0 +1,23 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entity.Concrete;
+
+namespace Business.Concrete
+{
+    public class FaqManager : IFaqService
+    {
+        private readonly IFaqDal faqDal;
+        public FaqManager(IFaqDal faqDal)
+        {
+            this.faqDal = faqDal;
+        }
+
+        public IDataResult<List<Faq>> GetAll()
+        {
+            var values = faqDal.GetAll();
+
+            return new SuccessDataResult<List<Faq>>(values);
+        }
+    }
+}
