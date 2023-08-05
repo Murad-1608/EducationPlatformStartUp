679d26b6e8adee71fe45e571696f13f10d0fcab3
Murad <murad.yunus.2017@gmail.com>
2023-08-05 23:35:09 +0400

    add faqAnswer table

---
+++ Entity/Concrete/FaqAnswer.cs
@@ -0,0 +1,13 @@
+﻿using Core.Entity.Abstract;
+
+namespace Entity.Concrete
+{
+    public class FaqAnswer : IEntity
+    {
+        public int Id { get; set; }
+        public int FaqId { get; set; }
+        public string Answer { get; set; }
+
+        public Faq Faq { get; set; }
+    }
+}
