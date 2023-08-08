694014959b787337f06cb9e5f8f4a9749a641be4
Murad <murad.yunus.2017@gmail.com>
2023-08-08 13:16:26 +0400

    Create question and answer table for support

---
+++ Entity/Concrete/SupportAnswer.cs
@@ -0,0 +1,13 @@
+﻿using Core.Entity.Abstract;
+
+namespace Entity.Concrete
+{
+    public class SupportAnswer : IEntity
+    {
+        public int Id { get; set; }
+        public int SupportQuestionId { get; set; }
+        public string Content { get; set; }
+
+        public SupportQuestion SupportQuestion { get; set; }
+    }
+}
---
+++ Entity/Concrete/SupportQuestion.cs
@@ -0,0 +1,12 @@
+﻿using Core.Entity.Abstract;
+
+namespace Entity.Concrete
+{
+    public class SupportQuestion : IEntity
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+
+        public SupportAnswer? SupportAnswer { get; set; }
+    }
+}
